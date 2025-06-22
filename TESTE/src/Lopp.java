6538f7654f4c479b042cd7f14226358cf463e239
EzekielRangel <ezekiel-6540128@educar.rs.gov.br>
2025-06-22 02:38:11 -0300

    Delete TESTE directory

--- TESTE/src/Lopp.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class Lopp {
-    public static void main(String[] args) {
-        int[] ide = {1,2,4};
-        ide[0] = 4;
-        System.out.println(Arrays.toString(ide));
-    }
-}
